9bd81366b0f06551bfacbf24fd3cde8bdb942257
박정우 <wjddnzzzz@naver.com>
2022-03-24 14:42:21 +0900

    teamproject html생성.

---
+++ src/main/java/com/example/teamwork_server/TeamworkServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamwork_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamworkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamworkServerApplication.class, args);
+    }
+
+}
